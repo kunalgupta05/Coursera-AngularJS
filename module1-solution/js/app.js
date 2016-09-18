ebd892c2e266418e257d54870477b4dd64907205
Kunal Gupta <kunal@github.com>
2016-09-19 01:36:38 +0530

    Made some changes to avoid flashes while the page  loads

--- module1-solution/js/app.js
+++ module1-solution/js/app.js
@@ -4,7 +4,8 @@
     angular.module('LunchCheck', [])
 
     .controller('LunchCheckController', LunchCheckController);
-
+    
+    //Using the $inject method to avoid the name shortening issues which can occur during the minification of the js file
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
         $scope.lunchItems = "";
@@ -17,7 +18,7 @@
         }
         $scope.message = "";
         $scope.emptyItemMessage="";
-        
+               
         $scope.getLunchItems = function () {
             $scope.emptyItemMessage="";
             var items = $scope.lunchItems;
@@ -33,13 +34,17 @@
                 }
                 if(emptyItemCount!==0){
                     $scope.emptyItemMessage=emptyItemCount+" empty items found in the list which are not counted in the actual count of the list."
-                }
-                if(items.length-emptyItemCount<=3){
+                }                
+                if(items.length-emptyItemCount>0 && items.length-emptyItemCount<=3){
                     $scope.message="Enjoy!";                                        
                 }
-                else{
+                else if(items.length-emptyItemCount>3){
                     $scope.message="Too much!";
                     $scope.lunchitemStyles.color="green";
+                }
+                else{
+                    $scope.message="";
+                    $scope.styleBorderColor['border-color']="red";
                 }                                 
             }
             else{
