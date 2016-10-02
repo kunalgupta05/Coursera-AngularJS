6143ea0afbeeacb8f41312aa5f07c50cdbabc4a9
Kunal Gupta <kunal@github.com>
2016-10-03 00:02:34 +0530

    half way there

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -1,80 +1,56 @@
 (function () {
     'use strict';
 
-    angular.module("ShoppingListCheckOff", [])
-        .controller("ToBuyShoppingController", ToBuyShoppingController)
-        .controller("AlreadyBoughtShoppingController", AlreadyBoughtShoppingController)
-        .service("ShoppingListCheckOffService", ShoppingListCheckOffService)
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com')
 
-    ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
-    function ToBuyShoppingController(ShoppingListCheckOffService) {
-        var buyItems = this;
+    NarrowItDownController.$inject = ['MenuSearchService']
+    function NarrowItDownController(MenuSearchService) {
+        var menu = this;
 
-        //Get the Pre-populated array for displaying it in the list of items to buy from the servie
-        buyItems.itemsArray = ShoppingListCheckOffService.getBuyItemsArray();
-        
-        //When the user clicks on the bought button it removes the item from the buy list and adds it to bought list
-        buyItems.removeItem = function (itemIndex) {
-            ShoppingListCheckOffService.removeFromBuyAddToBought(itemIndex);
-        }
+        menu.found = function (searchTerm) {
+            var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+            promise.then(function (response) {
+                console.log(response.data);
+            }).catch(function (error) {
+                console.log(error);
+            })
+        };
+
     }
 
-    AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
-    function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
-        var boughtItems = this;
-
-        //Get the items available in the boughtItems array from the service
-        boughtItems.itemsArray = ShoppingListCheckOffService.getBoughtItemsArray();
-    }
-
-    //ShoppingListService which contains our business logic used in both the controllers
-    function ShoppingListCheckOffService() {
+    MenuSearchService.$inject = ['$http', 'ApiBasePath']
+    function MenuSearchService($http,ApiBasePath) {
         var service = this;
 
-        // List of shopping items to be bought
-        var buyItems = [
-            {
-                name: "Cookies",
-                quantity: 10,
-            },
-            {
-                name: "Chips",
-                quantity: 5,
-            },
-            {
-                name: "Soda-cans",
-                quantity: 8,
-            },
-            {
-                name: "Gummy bears",
-                quantity: 6,
-            },
-            {
-                name: "Pepto Bismol",
-                quantity: 3,
-            }
-        ];
+        var foundItems = [];
 
-        //List of items which are already bought
-        var boughtItem = [];
-        
-        
-        //Return the pre-populated array to the ToBuyShoppingController
-        service.getBuyItemsArray = function () {
-            return buyItems;
+        service.getMatchedMenuItems = function (searchTerm) {
+            var promise = $http({
+                method: 'GET',
+                url: (ApiBasePath + '/menu_items.json'),
+            });
+                        
+            promise.then(function (response) {
+                console.log("I m in");
+                for (var i = 0; i < response.data.length; i++) {
+                    if (response.data.description == searchTerm) {
+                        foundItems.push(response.data[i]);
+                    }
+                }
+                
+                
+                return foundItems;
+            }).catch(function (error) {
+                return error;
+            })
         }
-
-        //Remove the items from the buyitems array and add the same item to the bought items array
-        service.removeFromBuyAddToBought = function (itemIndex) {
-            boughtItem.push(buyItems[itemIndex]);            
-            buyItems.splice(itemIndex, 1);
-        }
-
-        //Return the bought items array to the AlreadyBoughtShoppingController
-        service.getBoughtItemsArray = function () {
-            return boughtItem;
-        }
-
     }
+
+
+
 
 })();
