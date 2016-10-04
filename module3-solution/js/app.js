028087731cb940d05b275b225c5aa87c814f4708
Kunal Gupta <kunal@github.com>
2016-10-04 22:27:52 +0530

    Completed all the user stories for the assignment

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -5,27 +5,56 @@
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
         .constant('ApiBasePath', 'https://davids-restaurant.herokuapp.com')
-        .directive('foundItems', FoundItems)
+        .directive('foundItems', FoundItemsDirective)
 
-
-    function FoundItems(){
-        var ddo={
-            templateUrl:"foundItemsList.html",
-            scope:{
-                menu:'&found'
+    //Custom directive
+    function FoundItemsDirective() {
+        var ddo = {
+            templateUrl: "foundItemsList.html",
+            scope: {
+                found: "<",
+                onRemove: "&"
             }
         };
-        
         return ddo;
-    }    
-
+    }
+    
+   
     NarrowItDownController.$inject = ['MenuSearchService']
     function NarrowItDownController(MenuSearchService) {
         var menu = this;
 
-        menu.found = function (searchTerm) {
-            var foundItems = MenuSearchService.getMatchedMenuItems(searchTerm);
-            return foundItems;
+        menu.found = [];
+        menu.description = "";
+        menu.error = "";
+
+        menu.getMatchedMenuItems = function () {
+            if (menu.description.trim() !== "") {
+                menu.error = "";
+                var promise = MenuSearchService.getMatchedMenuItems(menu.description);
+
+                promise.then(function (response) {
+                    if (response.length == 0) {
+                        menu.found = [];
+                        menu.error = "Nothing found";
+                    }
+                    else {
+                        menu.found = response;
+                    }
+
+                }).catch(function (error) {
+                    console.log("Something went terribly wrong");
+                })
+            }
+            else {
+                menu.found = [];
+                menu.error = "Nothing found";
+            }
+
+        }
+
+        menu.removeItem = function (itemIndex) {
+            MenuSearchService.removeItem(itemIndex);
         };
 
     }
@@ -34,26 +63,30 @@
     function MenuSearchService($http, ApiBasePath) {
         var service = this;
 
-        service.getMatchedMenuItems = function (searchTerm) {
-            var foundItems = [];
-            var promise = $http({
-                method: 'GET',
-                url: (ApiBasePath + '/menu_items.json'),
-            });
+        var foundItems = [];
 
-            promise.then(function (response) {
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                method: 'GET',
+                url: (ApiBasePath + '/menu_items.json')
+            }).then(function (response) {
+                foundItems = [];
                 var re = new RegExp(searchTerm, "gi");
                 for (var i = 0; i < response.data.menu_items.length; i++) {
                     if (re.test(response.data.menu_items[i].description)) {
                         foundItems.push(response.data.menu_items[i]);
                     }
                 }
-                console.log(foundItems);
+                //console.log(foundItems);
                 return foundItems;
             }).catch(function (error) {
                 return error;
             })
         }
+
+        service.removeItem = function (itemIndex) {
+            foundItems.splice(itemIndex, 1);
+        };
     }
 
 
