52318df0f590f7f056570012b3dcd0c17a51bfef
Kunal Gupta <kunal@github.com>
2016-10-10 01:28:45 +0530

    Initial commit with the setup

---
+++ module4-solution/src/routes.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/templates/home.template.html'
+  })
+
+  // List of all menu items page
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/templates/categorieslist.template.html',
+    controller: 'CategoriesListController as categories'
+  });
+}
+
+})();
